bfa52e5811366ba3481e7e8344e7932e93df2e7b
Vagner Rods <vagner@rodrigues.sh>
2024-03-16 11:00:49 -0300

    General changes

---
+++ linux/server/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+sudo apt-get install zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# reboot
+
+p10k configure
